4a66bf5092eaf27cdce7dfce146346fb5fd8a1c5
domakun <http://52.194.207.217>
2019-08-30 18:45:27 +0800

    Enable message encryption

--- src/views/wallet/wallet-created/WalletCreatedTs.ts
+++ src/views/wallet/wallet-created/WalletCreatedTs.ts
@@ -1,134 +1,129 @@
-
-import {Message, networkType} from "@/config/index.ts"
-import {NetworkType} from "nem2-sdk"
-import {Component, Prop, Vue} from 'vue-property-decorator'
-import {strToHexCharCode} from '@/core/utils/utils.ts'
-import {createAccount, randomMnemonicWord} from "@/core/utils/hdWallet.ts"
-import {encryptKey, getAccountDefault, saveLocalWallet} from "@/core/utils/wallet.ts"
+import {Message, networkType} from "@/config/index.ts";
+import {NetworkType} from "nem2-sdk";
+import {Component, Prop, Vue} from 'vue-property-decorator';
+import {strToHexCharCode} from '@/core/utils/utils.ts';
+import {createAccount, randomMnemonicWord} from "@/core/utils/hdWallet.ts";
+import {encryptKey, getAccountDefault, saveLocalWallet} from "@/core/utils/wallet.ts";
 
 @Component
-export class WalletCreatedTs extends Vue{
+export class WalletCreatedTs extends Vue {
 
-    tags = 0
-    mosaics = []
-    storeWallet = {}
-    showCover = true
-    mnemonicRandomArr = []
+    tags = 0;
+    mosaics = [];
+    storeWallet = {};
+    showCover = true;
+    mnemonicRandomArr = [];
     formItem = {
         currentNetType: '',
         walletName: '',
         password: '',
         checkPW: '',
-    }
+    };
     netType = networkType;
 
 
-    @Prop({default:{}})
-    createForm: any
+    @Prop({default: {}})
+    createForm: any;
 
 
-    get mnemonic () {
-        const mnemonic = this.$store.state.app.mnemonic
-        return mnemonic['split'](' ')
+    get mnemonic() {
+        const mnemonic = this.$store.state.app.mnemonic;
+        return mnemonic['split'](' ');
     }
 
-    get formInfo () {
+    get formInfo() {
         return this.createForm;
     }
 
-    get node () {
-        return 'http://120.79.181.170:3000'
+    get walletList() {
+        return this.$store.state.app.walletList;
     }
 
-    get walletList () {
-        return this.$store.state.app.walletList
+    hideCover() {
+        this.showCover = false;
     }
 
-    hideCover () {
-        this.showCover = false
-    }
-
-    sureWord (index) {
-        const word = this.mnemonicRandomArr[index]
+    sureWord(index) {
+        const word = this.mnemonicRandomArr[index];
         const wordSpan = document.createElement('span');
         wordSpan.innerText = word;
         wordSpan.onclick = () => {
-            this.$refs['mnemonicWordDiv']['removeChild'](wordSpan)
-        }
-        this.$refs['mnemonicWordDiv']['append'](wordSpan)
+            this.$refs['mnemonicWordDiv']['removeChild'](wordSpan);
+        };
+        this.$refs['mnemonicWordDiv']['append'](wordSpan);
     }
 
-    checkMnemonic () {
+    checkMnemonic() {
         const mnemonicDiv = this.$refs['mnemonicWordDiv'];
         const mnemonicDivChild = mnemonicDiv['getElementsByTagName']('span');
-        let childWord = []
-        for(let i in mnemonicDivChild){
-            if( typeof mnemonicDivChild[i] !== "object") continue;
-            childWord.push(mnemonicDivChild[i]['innerText'])
+        let childWord = [];
+        for (let i in mnemonicDivChild) {
+            if (typeof mnemonicDivChild[i] !== "object") continue;
+            childWord.push(mnemonicDivChild[i]['innerText']);
         }
         if (JSON.stringify(childWord) != JSON.stringify(this.mnemonic)) {
             if (childWord.length < 1) {
-                this.$Notice.warning({title:''+this.$t(Message.PLEASE_ENTER_MNEMONIC_INFO)})
+                this.$Notice.warning({title: '' + this.$t(Message.PLEASE_ENTER_MNEMONIC_INFO)});
             } else {
-                this.$Notice.warning({title:''+this.$t(Message.MNEMONIC_INCONSISTENCY_ERROR)})
+                this.$Notice.warning({title: '' + this.$t(Message.MNEMONIC_INCONSISTENCY_ERROR)});
             }
-            return false
+            return false;
         }
-        return true
+        return true;
     }
 
-    changeTabs (index) {
+    changeTabs(index) {
         switch (index) {
             case 0:
-                this.tags = index
+                this.tags = index;
                 break;
             case 1:
-                this.mnemonicRandomArr = randomMnemonicWord(this.mnemonic)
-                this.tags = index
+                this.mnemonicRandomArr = randomMnemonicWord(this.mnemonic);
+                this.tags = index;
                 break;
             case 2:
-                if(!this.checkMnemonic()){
-                    return
+                if (!this.checkMnemonic()) {
+                    return;
                 }
-                const account = createAccount(this.mnemonic.join(' '))
-                this.$store.commit('SET_ACCOUNT',account);
-                this.loginWallet(account)
-                this.tags = index
+                const account = createAccount(this.mnemonic.join(' '));
+                this.$store.commit('SET_ACCOUNT', account);
+                this.loginWallet(account);
+                this.tags = index;
                 break;
         }
     }
 
-    skipInput (index) {
-        const account = createAccount(this.mnemonic.join(' '))
-        this.$store.commit('SET_ACCOUNT',account);
-        this.loginWallet(account)
-        this.tags = index
+    skipInput(index) {
+        const account = createAccount(this.mnemonic.join(' '));
+        this.$store.commit('SET_ACCOUNT', account);
+        this.loginWallet(account);
+        this.tags = index;
     }
 
 
-    loginWallet (account) {
-        const that = this
-        const walletName:any = this.formInfo['walletName'];
-        const netType:NetworkType = Number(this.formInfo['currentNetType'])
-        const walletList = this.$store.state.app.walletList
-        const style = 'walletItem_bg_' + walletList.length % 3
-        getAccountDefault(walletName, account, netType).then((wallet)=>{
-            let storeWallet = wallet
-            storeWallet['style'] = style
-            that.storeWallet = storeWallet
-            that.$store.commit('SET_WALLET', storeWallet)
-            const encryptObj = encryptKey(storeWallet['privateKey'], that.formInfo['password'])
-            const mnemonicEnCodeObj = encryptKey(strToHexCharCode(this.mnemonic.join(' ')), that.formInfo['password'])
-            saveLocalWallet(storeWallet, encryptObj, null, mnemonicEnCodeObj)
-        })
+    loginWallet(account) {
+        const that = this;
+        const walletName: any = this.formInfo['walletName'];
+        const netType: NetworkType = Number(this.formInfo['currentNetType']);
+        const walletList = this.$store.state.app.walletList;
+        const style = 'walletItem_bg_' + walletList.length % 3;
+        getAccountDefault(walletName, account, netType).then((wallet) => {
+            let storeWallet = wallet;
+            storeWallet['style'] = style;
+            that.storeWallet = storeWallet;
+            that.$store.commit('SET_WALLET', storeWallet);
+            const encryptObj = encryptKey(storeWallet['privateKey'], that.formInfo['password']);
+            const mnemonicEnCodeObj = encryptKey(strToHexCharCode(this.mnemonic.join(' ')), that.formInfo['password']);
+            saveLocalWallet(storeWallet, encryptObj, null, mnemonicEnCodeObj);
+        });
     }
 
-    toWalletPage () {
-        this.$store.commit('SET_HAS_WALLET',true)
-        this.$emit('toWalletDetails')
+    toWalletPage() {
+        this.$store.commit('SET_HAS_WALLET', true);
+        this.$emit('toWalletDetails');
     }
 
-    toBack () {
-        this.$emit('closeCreated')
+    toBack() {
+        this.$emit('closeCreated');
     }
 }
