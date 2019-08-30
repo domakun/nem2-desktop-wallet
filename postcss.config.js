5c2d8f308e9b688ad5695f4cd370af5d1fd4ce62
dnice <dnice_bo@Outlook.com>
2019-08-30 19:14:16 +0800

    fix bug package.ts

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,14 @@
-const isWin32 = require('./src/config/packge.js').isWin32
+const isWin32 = require('./src/config/packge.ts').isWin32
 const propList = isWin32 ? ["*", '!font-size'] : ["*"]
 
 module.exports = {
-  plugins: {
-    "postcss-import": {},
-    "autoprefixer": {},
-    "postcss-pxtorem": {
-      "rootValue": 100,
-      "propList": propList,
-      "selectorBlackList": ["mint-"],
+    plugins: {
+        "postcss-import": {},
+        "autoprefixer": {},
+        "postcss-pxtorem": {
+            "rootValue": 100,
+            "propList": propList,
+            "selectorBlackList": ["mint-"],
+        }
     }
-  }
 }
