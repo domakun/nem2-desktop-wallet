eec7a00c284552861e7bf8f7c7a6efa6bbfe0576
domakun <http://52.194.207.217>
2019-08-31 12:34:24 +0800

    update vee-validate

--- src/views/monitor/monitor-transfer/transactions/transfer-transaction/TransferTransaction.vue
+++ src/views/monitor/monitor-transfer/transactions/transfer-transaction/TransferTransaction.vue
@@ -52,17 +52,19 @@
         </span>
       </div>
 
-      <ErrorTooltip fieldName="mosaicListLength" placementOverride="top">
-        <input
-                data-vv-name="mosaicListLength"
-                number
-                type="text"
-                v-validate="standardFields.mosaicListLength.validation"
-                style="display: none"
-                v-model="formModel.mosaicTransferList.length"
-        />
-      </ErrorTooltip>
+
       <div class="mosaic_list_container radius ">
+        <ErrorTooltip fieldName="mosaicListLength" placementOverride="top">
+          <input
+                  data-vv-name="mosaicListLength"
+                  number
+                  type="text"
+                  v-validate="standardFields.mosaicListLength.validation"
+                  style="display: none"
+                  v-model="formModel.mosaicTransferList.length"
+          />
+        </ErrorTooltip>
+
         <span class="mosaic_name overflow_ellipsis">{{$t('mosaic')}}</span>
         <span class="mosaic_amount overflow_ellipsis">{{$t('amount')}}</span>
         <div class="scroll">
@@ -148,7 +150,7 @@
 </template>
 
 <script lang="ts">
-    import TransferTransactionTs from './TransferTransactionTs';
+    import TransferTransactionTs from './TransferTransactionTs'
 
     export default class TransferTransaction extends TransferTransactionTs {
     }
