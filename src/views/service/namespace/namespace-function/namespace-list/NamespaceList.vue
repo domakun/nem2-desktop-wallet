244fb950b3c7e3da6f26b70a03e571d577449e26
domakun <http://52.194.207.217>
2019-09-26 10:24:40 +0800

    merge conflects

--- src/views/service/namespace/namespace-function/namespace-list/NamespaceList.vue
+++ src/views/service/namespace/namespace-function/namespace-list/NamespaceList.vue
@@ -1,66 +1,68 @@
 <template>
   <div class="namespace_list_container secondary_page_animate">
-    <Spin v-if="namespaceLoading" size="large" fix class="absolute"></Spin>
-    <div class="top_text">
-      <div class="head_title">{{$t('Update_namespace_prompts')}}</div>
-      <div class="tips">{{$t('namespace_list_tips_1')}}</div>
-      <div class="tips">{{$t('namespace_list_tips_2')}}</div>
-    </div>
-    <div class="namespace_list_table">
+
+    <div class="left_container">
+      <Spin v-if="namespaceLoading" size="large" fix class="absolute"></Spin>
+      <div class="top_text">
+        <div class="head_title">{{$t('Update_namespace_prompts')}}</div>
+        <div class="tips">{{$t('namespace_list_tips_1')}}</div>
+        <div class="tips">{{$t('namespace_list_tips_2')}}</div>
+      </div>
+      <div class="namespace_list_table">
 
 
-      <div class="table_head">
+        <div class="table_head">
         <span @click="getSortType(namespaceSortType.byName)" class="namesapce_name">
           {{$t('namespace_name')}}
           <Icon :class="namespaceSortType.byName == currentSortType?'active_sort_type':''" type="md-arrow-round-down"/>
         </span>
-        <span @click="getSortType(namespaceSortType.byDuration)" class="duration">
+          <span @click="getSortType(namespaceSortType.byDuration)" class="duration">
           {{$t('duration')}}
              <Icon :class="namespaceSortType.byDuration == currentSortType?'active_sort_type':''"
                    type="md-arrow-round-down"/>
         </span>
-        <span @click="getSortType(namespaceSortType.byOwnerShip)" class="is_active">
+          <span @click="getSortType(namespaceSortType.byOwnerShip)" class="is_active">
           {{$t('Control')}}
              <Icon :class="namespaceSortType.byOwnerShip == currentSortType?'active_sort_type':''"
                    type="md-arrow-round-down"/>
         </span>
-        <span @click="getSortType(namespaceSortType.byBindType)" class="link">
+          <span @click="getSortType(namespaceSortType.byBindType)" class="link">
           {{$t('link')}}
              <Icon :class="namespaceSortType.byBindType == currentSortType?'active_sort_type':''"
                    type="md-arrow-round-down"/>
         </span>
-        <span @click="getSortType(namespaceSortType.byBindInfo)" class="type">
+          <span @click="getSortType(namespaceSortType.byBindInfo)" class="type">
           {{$t('type')}}
              <Icon :class="namespaceSortType.byBindInfo == currentSortType?'active_sort_type':''"
                    type="md-arrow-round-down"/>
         </span>
-        <span class="more"></span>
+          <span class="more"></span>
 
 
-        <div class="namespace_filter" @click="toggleIsShowExpiredNamesapce()">
-          <img v-if="!isShowExpiredNamesapce" src="@/common/img/window/windowSelected.png">
-          <img v-else src="@/common/img/window/windowUnselected.png">
-          <span>{{$t('Hide_expired_namespaces')}}</span>
+          <div class="namespace_filter" @click="toggleIsShowExpiredNamesapce()">
+            <img v-if="!isShowExpiredNamesapce" src="@/common/img/window/windowSelected.png">
+            <img v-else src="@/common/img/window/windowUnselected.png">
+            <span>{{$t('Hide_expired_namespaces')}}</span>
+          </div>
+
+
         </div>
-
-
-      </div>
-      <Spin v-if="namespaceLoading" size="large" fix class="absolute"></Spin>
-      <div class="table_body ">
-        <div class=" radius"
-             v-for=" n in currentNamespaceListByPage">
-          <div v-if="n" class="table_body_item">
-            <span class="namesapce_name overflow_ellipsis">{{n.label}}</span>
-            <span class="duration overflow_ellipsis">
+        <Spin v-if="namespaceLoading" size="large" fix class="absolute"></Spin>
+        <div class="table_body ">
+          <div class=" radius"
+               v-for=" n in currentNamespaceListByPage">
+            <div v-if="n" class="table_body_item">
+              <span class="namesapce_name overflow_ellipsis">{{n.label}}</span>
+              <span class="duration overflow_ellipsis">
               {{computeDuration(n) === StatusString.EXPIRED ? $t('overdue') : durationToTime(n.endHeight)}}
             </span>
-            <span class="is_active overflow_ellipsis">
+              <span class="is_active overflow_ellipsis">
             <Icon v-if="n.isActive" type="md-checkmark"/>
             <Icon v-else type="md-close"/>
             </span>
-            <span class="link overflow_ellipsis">{{n.aliasType}}</span>
-            <span class="type overflow_ellipsis">{{n.aliasTarget}}</span>
-            <span class="more overflow_ellipsis">
+              <span class="link overflow_ellipsis">{{n.aliasType}}</span>
+              <span class="type overflow_ellipsis">{{n.aliasTarget}}</span>
+              <span class="more overflow_ellipsis">
             <Poptip class="poptip_container" placement="top-end">
               <i class="moreFn"></i>
               <div slot="content" max-width="150" class="refresh_sub_container">
@@ -88,18 +90,20 @@
           </div>
           </Poptip>
           </span>
+            </div>
+          </div>
+
+          <div v-if="namespaceList.length == 0" class="noData">
+            <p>{{$t('no_data')}}</p>
           </div>
         </div>
+      </div>
 
-        <div v-if="namespaceList.length == 0" class="noData">
-          <p>{{$t('no_data')}}</p>
-        </div>
+      <div class="page_list_container">
+        <Page :total="currentNamespacelist.length" :page-size="pageSize" @on-change="handleChange"></Page>
       </div>
     </div>
 
-    <div class="page_list_container">
-      <Page :total="currentNamespacelist.length" :page-size="pageSize" @on-change="handleChange"></Page>
-    </div>
     <NamespaceEditDialog
             :currentNamespace="currentNamespace"
             :showNamespaceEditDialog="showNamespaceEditDialog"
